08cb0ba5d812e0b6bcc71eca892e3d1edba15e7a
RomanCherkasov <palkamon@yandex.ru>
2021-06-01 19:40:59 +0700

    edit settings and urls for upload and display images

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -59,6 +59,9 @@ MIDDLEWARE = [
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 LOGIN_URL = "/auth/login/"
 LOGIN_REDIRECT_URL = "index"
 
