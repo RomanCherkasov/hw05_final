2ec55f21a7bd670ffdfec2eaa6e8873188ca2324
RomanCherkasov <palkamon@yandex.ru>
2021-06-01 19:36:12 +0700

    migrations for the images library add fields for upload images to posts model and form

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -5,7 +5,9 @@ from django.forms import ModelForm
 class PostForm(ModelForm):
     class Meta:
         model = Post
-        fields = ['text', 'group']
+        fields = ['text',
+                  'group',
+                  'image',]
         labels = {'text': 'Текст статьи',
                   'group': 'Группа', }
         help_texts = {'text': 'Введите текст статьи',
